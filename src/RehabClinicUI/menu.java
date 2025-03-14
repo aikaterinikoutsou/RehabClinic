15c1a86c3034528bf8bac9aef42334a15aae95a0
aikaterinikoutsou <aikaterini.koutsou@ceu.es>
2025-03-14 19:51:31 +0100

    introduce first local change

--- src/RehabClinicUI/menu.java
+++ src/RehabClinicUI/menu.java
@@ -5,6 +5,7 @@ public class menu {
 	public static void main(String[] args) {
 		// TODO Auto-generated method 
 		System.out.println("Hello World");
+		System.out.println("Change 1 in local");
 
 	}
 
